081e6e8d035f25630db461a9461863bdbb152cbf
Pack Typebot <mathuza@gmail.com>
2023-12-06 07:55:02 -0300

    Update script.js

--- whatsapp/script.js
+++ whatsapp/script.js
@@ -101,7 +101,7 @@ if (!document.getElementById(cssId))
     link.id   = cssId;
     link.rel  = 'stylesheet';
     link.type = 'text/css';
-    link.href = 'https://templates.ideiaschatgpt.com.br/whatsapp/style.css';
+    link.href = 'https://raw.githubusercontent.com/mathuzabr/template/main/whatsapp/style.css';
     link.media = 'all';
     head.appendChild(link);
 }
